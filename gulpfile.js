375ba861c5268d7377638f5dabbaa85161e609ac
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-07-14 23:45:05 -0400

    solarized light highlightjs theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+// var autoprefixer = require('gulp-autoprefixer');
 
 // 一次性编译 Sass 
 gulp.task('sass', function() {
     return gulp.src('./source/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(autoprefixer())
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        // .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
 });
 
